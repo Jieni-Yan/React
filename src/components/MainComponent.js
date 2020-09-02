d61b3efcbf1208f7af046e173d2a4f302633f68e
Jieni Yan <jennyyan54@gmail.com>
2020-09-02 18:53:43 +0800

    React Router and Single Page Applications

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,7 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-
+import About from "./AboutComponent";
 import Home from './HomeComponent';
 import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
@@ -50,6 +50,7 @@ class Main extends Component {
         <Header />
         <Switch>
             <Route path="/home" component={HomePage} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
             <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
